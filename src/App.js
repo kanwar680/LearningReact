25eb86278ddd6578cabfa5ad272c15da445b519d
kanwar680 <87905132+kanwar680@users.noreply.github.com>
2021-07-24 16:55:56 +0200

    ChangeFrom Sandbox

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>TestGithub</h2>
+      <h2>TestGithub - change in CodeSandbox</h2>
     </div>
   );
 }
